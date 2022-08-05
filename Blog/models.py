912bddfaa614ee4664097dac85813e698fdc8945
yohan <noyohanx@naver.com>
2022-08-05 21:32:52 +0900

    ✨ : Study Django Model

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,20 @@
+from curses import ACS_GEQUAL
+from tkinter.simpledialog import askinteger
+from venv import create
 from django.db import models
+from django.forms import DateTimeField
 
-# Create your models here.
+class Member(models.Model):
+    memberName = models.CharField(max_length=10)
+    password = models.CharField(max_length=15)
+    name = models.CharField(max_length=12)
+    age = models.CharField(max_length=3)
+    createDate = models.DateTimeField(auto_now_add=True)
+    updateDate = models.DateTimeField(auto_now=True)
+
+class Board(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.CharField(max_length=256)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    createDate = DateTimeField(auto_now_add = True)
+    updateDate = DateTimeField(auto_now = True)

cabbfe25ede3cf35a6f4c1b6e46b33e07f047022
yohan <noyohanx@naver.com>
2022-08-09 13:51:32 +0900

    🔥 : 오류로 인해 한번 다 밀었음

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,5 @@ from Blog import views
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.hello, name = 'hello'),
-    path('/login', views.login, name = 'login'),
+   
 ]

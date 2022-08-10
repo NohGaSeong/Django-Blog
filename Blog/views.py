06652038efc660d9a9eb844d5e9e63704f8a914c
yohan <noyohanx@naver.com>
2022-08-10 14:45:36 +0900

    ✨ : Add signup -> login

--- Blog/views.py
+++ Blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import is_valid_path
 from .form import MemberForm, SignupForm
 
 def hello(request):
@@ -12,5 +13,10 @@ def login(request):
     return render(request, 'login.html', {'form':form})
 
 def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:login')
     form = SignupForm()
     return render(request, 'signup.html', {'form':form})
--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from Blog import views
 from django.urls import path
 from django.contrib import admin
 
+app_name = 'blog'
 urlpatterns = [
    path('',views.hello, name = 'hello'),
    path('admin/', admin.site.urls),
